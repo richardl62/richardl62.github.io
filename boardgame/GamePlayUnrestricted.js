b14fc5a81b68baf5e0b4652b404627c6f484af3e
slightly-nerdy <github@slightlynerdy.org>
2020-03-21 13:31:37 +0000

    Add missing file

---
+++ boardgame/GamePlayUnrestricted.js
@@ -0,0 +1,39 @@
+/*
+ othello.js:  Code that is specific to Othello (but follows a pattern shared by other board games)
+*/
+"use strict";
+
+class GamePlayUnrestricted {
+    constructor(board) {
+        this.board = board;
+    }
+
+    static mode()
+    {
+        return "unrestricted";
+    }
+
+    static starting_positions_json()
+    {
+        return [
+            ["8x8", "[8,8,[[-1,-1,-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1,-1,-1]]]"],
+            ["1x1 (test)", "[1,1,[[-1]]]"],
+        ]
+    };
+
+    // Updates board squares.
+    // Returns true if the next player should have the next move
+    move(player, square) {
+        if(square.status().is_empty())
+        {
+            square.player(player);;
+            return true;
+        }
+         
+        return false;
+    }
+
+    display_status(player) {
+        $("#status").html(status_span("Player " + player, player));
+    }
+}
