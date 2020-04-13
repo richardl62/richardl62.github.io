b6c07f0b27b2ff7706c45a5cb7208fe466b8051a
slightly-nerdy <github@slightlynerdy.org>
2020-04-13 11:19:13 +0100

    Add cjson_stringify and cjson_parse

--- games/boardgame-beta/tools.js
+++ games/boardgame-beta/tools.js
@@ -19,5 +19,45 @@ function flatten(arr) {
     return [].concat.apply([], arr);
 }
 
+// cjson -> compact JSON - as JSON but 'null' removed. 
+function cjson_stringify(object)
+{
+    const json = JSON.stringify(object);
+    // console.log("json", json);
+    return json.replace(/null/g,'');
+}
 
+// restore object created by cjson_stringify. (Not perfect, but good enough for current purposes.)
+function cjson_parse(cjson)
+{
+    // A ',' after a special character is replaced by ',null'.
+    function is_special(c1, c2)
+    {
+        if(c2 == ',')
+            return c1 == ',' || c1 == '[';
+        if(c2 == ']')
+            return c1 == ',';
+    }
+
+    const chars = cjson.split('');
+    
+    var json = '';
+    var last_ch = false;
+
+    chars.forEach((c) => {
+        if(is_special(last_ch, c))
+            json+='null';
+          
+        json += c
+        last_ch = c;
+    });
+    // console.log("processed string", json);
+    return JSON.parse(json);
+}
+
+// const object = [,1,,2,];
+// console.log("object", object, "(length", object.length, ")");
+// const cjson = cjson_stringify(object);
+// console.log("object stringified", cjson);
+// console.log("string parsed", cjson_parse(cjson));
 
