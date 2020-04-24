b054cdb8461dfa7a3af5308597568e64532f9271
slightly-nerdy <github@slightlynerdy.org>
2020-04-24 12:19:38 +0100

    wip on Can't Stop game interaction

--- games/cant-stop/cant-stop.js
+++ games/cant-stop/cant-stop.js
@@ -10,6 +10,8 @@ const jq = {
     dont: $("#dont"),
     move_options: $("#move-options td"), 
     // num_players: $("#num-players"),
+    required_roll: $("#required-roll"),
+    restart: $("#restart"),
     roll: $("#roll"),
 }
 
@@ -19,9 +21,10 @@ for (const [key, value] of Object.entries(jq)) {
 }
 
 /*
- * Global variables (other the jq)
+ * Global variables (other the jq) and setup
  */
 
+const n_players = 2; // For now at least.
 const n_dice = 4;
 assert(jq.dice.length == n_dice, "4 dice expected");
 
@@ -33,11 +36,14 @@ let dice_array = make_dice_array();
 
 let controls_visibility = new SetVisiblity(jq.controls);
 let bust_visibility = new SetVisiblity(jq.bust);
-bust_visibility.off();
+let required_roll_visibility = new SetVisiblity(jq.required_roll)
 
+let move_options = []; // WIP
+let selected_move_option = 0; // WIP
 
+restart();
 /*
- * 'make' functions
+ * helper functions
  */
 
 function make_game_board() {
@@ -82,11 +88,11 @@ function make_game_board() {
             
         squares[0].css("border-top-style", "solid");
 
+        board.add_column(cn, squares.reverse());
         jq.board.append(col);
 
         n_squares += (cn < 7) ? 2 : -2;
     }
-
     return board;
 }
 
@@ -101,20 +107,74 @@ function make_dice_array() {
     return arr;
 }
 
-/*
+function make_visible(visible)
+{
+    bust_visibility.off();
+    controls_visibility.off();
+    required_roll_visibility.off();
+
+    visible.on();
+}
+
+
+ function do_roll(spin)
+ {
+    assert(spin != undefined, "spin option not set");
+
+    dice_array.forEach((d)=>d.roll(spin));
+    
+    let dice_numbers = [];
+    dice_array.forEach((d)=> dice_numbers.push(d.number()));
+
+    move_options = game_board.options(dice_numbers);
+    if(move_options.length == 0)
+    {
+        make_visible(bust_visibility);
+    }
+ }
+
+ function restart()
+ {
+    game_board.start_game(n_players);
+    make_visible(required_roll_visibility);
+ }
+
+ /*
  * Game interaction
  */
 
-jq.roll.click(() => {
-    dice_array.forEach((d)=>d.roll());
+jq.roll.click(function(elem){
+    do_roll(true /*spin*/);
 });
 
-jq.dont.click(()=>{
-    controls_visibility.off();
-    bust_visibility.on();
-})
+jq.required_roll.click(function(elem){
+    make_visible(controls_visibility);
+    do_roll(true /*spin*/); 
+});
 
-jq.bust.click(()=>{
-    controls_visibility.on();
-    bust_visibility.off();
-})
+jq.move_options.click(function (elem) {
+    // TEMPORARY KLUDGE
+    let move = move_options[selected_move_option];
+    ++selected_move_option;
+    if(selected_move_option == move_options.length)
+        selected_move_option = 0;
+
+    console.log("move:",move);
+    // END OF TEMPORARY KLUDGE
+
+    game_board.clear_last_pre_commit();
+    game_board.pre_commit(move);
+
+});
+
+jq.dont.click(function(elem){
+    game_board.commit();
+});
+
+jq.bust.click(function(elem){
+    make_visible(required_roll_visibility);
+});
+
+jq.restart.click(function(elem){
+    restart()
+});
