cbb79a84c218078630542a2e95dc3292d9831cba
slightly-nerdy <github@slightlynerdy.org>
2020-06-24 20:09:04 +0100

    Work on networking controls in index.html

--- games/index.js
+++ games/index.js
@@ -1,67 +1,25 @@
 'use strict'
+const game_name_elems = document.querySelectorAll('input[name="game"]');
 const game_id_elem  = getElementById_Checked("game-id");
+const local_server_elem  = getElementById_Checked("local-server");
 const play_offline_elem = getElementById_Checked("play-offline");
 const start_elem = getElementById_Checked("start");
-const show_open_games_elem = getElementById_Checked("show-open-games");
-const open_games_list = getElementById_Checked("open-games-list");
+const refresh_open_games_elem = getElementById_Checked("refresh-open-games");
+const clear_open_games_elem = getElementById_Checked("clear-open-games");
+const open_games_info = getElementById_Checked("open-games-info");
 
-
-
-function start_game(id, type) {
-
-  const data = { 
-    id: id,
-    type: type 
-  };
-
-  const fetch_options = {
-    // method: 'POST',
-    // headers: {
-    //   'Content-Type': 'application/json',
-    // },
-    // body: JSON.stringify(data),
-  };
-
-  fetch('http://localhost:5000/start-game', fetch_options)
-    .then(response => response.json())
-    .then(data => {
-      console.log('Success:', data);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
+function get_server() {
+  if(local_server_elem.checked)
+    return "http://localhost:5000/";
+  else
+    return "http://madeup.com/";
 }
+const online_games = [ "dropdown" ];
 
-//start_game("game1", "downfall")
-
-function show_open_games() {
-  fetch('http://localhost:5000/open-games').then(function(response) {
-    return response.json()
-  }).then(function(data) {
-    let html = "";
-    for(let [id, type] of data) {
-      html += `<p>${id} ${type}<\p>\n`
-    }
-    
-    open_games_list.innerHTML = html;
-  }).catch(err => console.log("Error: ",err));
-}
-//show_open_games();
-
-// start_elem.addEventListener("click", (e) => {
-//   get_open_games();
-// })
-
-// const games = document.querySelectorAll('input[name="game"]');
-
-// const onlineGames = [ "dropdown" ];
-
-
-// function UpdateGameVisibilty() {
-//     const online_only = new_game.checked || game_id.value != "";
-
+// function showOnlyOnlineGames(online_only) {
+ 
 //     let default_checked = false;
-//     for (let game of games) {
+//     for (let game of show_latest_online_game) {
 //         const show = !online_only || onlineGames.includes(game.value);
 //         game.parentElement.style.display = show ? "initial" : "none";
 
@@ -74,48 +32,177 @@ function show_open_games() {
 //     }
 // }
 
+// id is optional. If not wanted can be suppied as null
+function game_href(id, game) {
+  assert(game);
+  
+  let href;
+  if (game == "dropdown" || game == "othello") {
+    href = "boardgame/boardgame.html"
+  } else if (game == "dice") {
+    href = "dice/dice-game.html"
+  } else if (game == "cantstop") {
+    href = "cant-stop/cant-stop.html"
+  } else if (game == "plusminus") {
+    game_display_name = "Plus/Minus"
+  }
+  assert(href);
+
+  let search_params = new URLSearchParams;
+  search_params.set("game", game);
+
+  if (id) {
+    search_params.set("id", id);
+  }
+
+  href += "?" + search_params.toString();
+
+  return href;
+}
+
+function game_display_name(game) {
+  assert(game);
+
+  let display_name;
+  
+  if (game == "cantstop") {
+    display_name = "Can't Stop"
+  } else if (game == "plusminus") {
+    display_name = "Plus/Minus"
+  } else {
+    // Capitalise the first letter - This is the default
+    display_name = game.charAt(0).toUpperCase() + game.slice(1);
+  }
+
+  return display_name;
+}
+
+class OnlineGameInfo {
+  constructor() {
+    this.no_games();
+  }
+
+  reset(class_name) {
+    open_games_info.innerHTML = "";
+    open_games_info.className = class_name ? class_name : "";
+  }
+
+
+  no_games() {
+    this.reset('open-games-no-games');
+    open_games_info.innerHTML = "<div>No games found</div>";
+  }
+
+  error(...lines) {
+    this.reset('open-games-error');
+
+    for (let line of lines) {
+      if(line instanceof Error) {
+        console.log("OnlineGameInfo::error", line);
+      }
+      open_games_info.innerHTML += `<div>${line}</div>\n`;
+    }
+  }
+
+  add_first_game(id, game) {
+    if(open_games_info.className != 'open-games-list') {
+      this.reset('open-games-list');
+    }
+
+    const href = game_href(id, game);
+    const display_name = game_display_name(game);
+
+    const link = `<a href="${href}">${id}</a>`;
+    const span = `<span>${display_name}</span>\n`;
+
+    open_games_info.innerHTML = link + span + open_games_info.innerHTML;
+  }
+}
+
+let oneline_game_info = new OnlineGameInfo;
+
+
+function show_all_open_games() {
+  fetch(get_server() + 'open-games')
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (data) {
+        oneline_game_info.reset();
+        for (let [id, game] of data) {
+          oneline_game_info.add_first_game(id, game);
+        }
+    })
+    .catch(err => oneline_game_info.error("Can't retrieve open games", err));
+}
+
+function start_game(id, game_type) {
+  const data = { 
+    id: id,
+    game: game_type, 
+  };
+
+  const fetch_options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  };
+
+  fetch(get_server() + 'start-game', fetch_options)
+    .then(response => response.json())
+    .then(data => {
+      oneline_game_info.add_first_game(id, game_type);
+    })
+    .catch((error) => {
+      console.error('Error:', error.message);
+    });
+}
+
+
+function selected_game() {
+  for(let elem of game_name_elems) {
+    if(elem.checked)
+      return elem.value;
+  }
+  assert(false);
+}
+
+start_elem.addEventListener("click", (e) => {
+  start_game(game_id_elem.value, selected_game());
+})
+
+refresh_open_games_elem.addEventListener("click", (e) => {
+  show_all_open_games();
+})
+
+clear_open_games_elem.addEventListener("click", (e) => {
+  fetch(get_server() + 'clear')
+    .then(response => {
+      console.log("response", response)
+      return response.text();
+    })
+    .then(data => {
+      console.log("data", data)
+      show_all_open_games()
+    })
+    .catch(err => {
+      console.log("err", err)
+      oneline_game_info.error("Problem clearing open games", err);
+    })
+})
+
 play_offline_elem.addEventListener("click", (e) => {
-    const game = document.querySelector('input[name="game"]:checked').value;
-
-    let path;
-    if(game == "dropdown" || game == "othello") {
-      path = "boardgame/boardgame.html" 
-    } else if (game == "dice") {
-      path = "dice/dice-game.html"
-    } else if (game == "cantstop") {
-      path = "cant-stop/cant-stop.html"
-    } else if (game == "plusminus") {
-      path= "plus-minus/plus-minus.html"
-    }
-    
- 
-    if(path) {
-      let search_params = new URLSearchParams;
-      search_params.set("game", game);
-
-      window.location.href = path + "?" + search_params.toString();
-    }
-    else {
-        alert(`game "${game}" is not recognised`);
-    }
+    const id = null;
+    const game = selected_game();
+    window.location.href  = game_href(id, game);
 });
 
-// game_id.onchange = function(event) {
-//     console.log("game_id", game_id.value);
-//     if(game_id.value) {
-//         new_game.checked = false;
-//     }
-//     UpdateGameVisibilty();
-// };
-
-// new_game.onchange = function(event) {
-//     if(new_game.checked) {
-//         game_id.value = "";
-//     }
-
-//     UpdateGameVisibilty();
-// };
-
+local_server_elem.addEventListener("change", (e) => {
+  show_all_open_games();
+});
+//show_all_open_games();
 
 
 
