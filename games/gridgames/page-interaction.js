864d5a60343a4fe5da71ddc9a08651ef9fe04e83
slightly-nerdy <github@slightlynerdy.org>
2020-07-03 11:35:21 +0100

    Remove experimental gridgame features

--- games/gridgames/page-interaction.js
+++ games/gridgames/page-interaction.js
@@ -5,18 +5,14 @@ const jq = { // Get the jQuery elements that are used in this file.
     clear: $("#clear"),
     customise_button: $("#customise-button"),
     customise_menu: $("#customise-menu"),
-    experimental: $("#experimental"),
-    export: $("#export"),
     game_option: $("#game-option"),
     game_type: $("#game-type"),
-    link: $("#link"),
     num_cols:  $("#num-cols"),
     num_players: $("#num-players"),
     num_rows:  $("#num-rows"),
     pass: $("#pass"),
     redo: $("#redo"),
     restart: $("#restart"),
-    full_width: $("#full-width"),
     online_status: $("#online-status"),
     scores: $("#scores"),
     status_message: $("#status-message"),
@@ -87,84 +83,3 @@ jq.num_players.change(()=>{
     game_control.num_players(n_rows);
 });
 
-jq.export.click(()=>{
-    var stringified = stringify_array(
-        game_control.board_status(),
-        (elem) => elem === null ? "" : elem
-        );
-        
-    var new_window = window.open("", "");
-    new_window.document.write("<p>" + stringified + "</p>");
-});
-
-jq.link.click(() => {
-    if (true) {
-        alert("Making a game link is not currently working");
-    } else {
-        let urlParams = new URLSearchParams();
-        urlParams.set("g", game_control.game_name());
-
-        const status = game_control.board_status();
-        urlParams.set("b", convert_board_status_for_url(status));
-
-        window.open(window.location.pathname + "?" + urlParams.toString());
-    }
-});
-
-// To do: Consider tidying thia full width code
-function set_full_width_options()
-{
-    var full_width = game_control.full_width();
-    $("body").css("width", full_width ? "100%" : "auto");
-    jq.full_width.toggleClass("button-pressed", full_width);
-}
-
-set_full_width_options();
-
-jq.full_width.click(function()
-{
-    game_control.full_width(!game_control.full_width()); //toggle
-    set_full_width_options();
-
-});
-
-function convert_board_status_for_url(board_status) {
-    let str = "";
-    board_status.forEach(row => {
-        // console.log(row);
-        row.forEach(sq =>{
-            assert(sq == null || (sq >= 0 && sq <= 9));
-           str += sq === null ? "." : sq; 
-        })
-        str += "-";
-    });
-
-    // Remove the final "-"
-    return str.slice(0, -1);
-}  
-
-function convert_row_from_url(row)
-{
-    let result = [];
-    row.split('').forEach(c => {
-        if(c == ".")
-            result.push(null);
-        else
-        {
-            const sq = parseInt(c);
-            assert(!isNaN(sq), "bad character: " + c);
-            result.push(sq);
-        }
-    });
-    return result;
-}
-
-function convert_board_status_from_url(str)
-{
-    let result = [];
-    str.split("-").forEach(row => {
-        result.push(convert_row_from_url(row));
-    });
- 
-    return result;
-}
