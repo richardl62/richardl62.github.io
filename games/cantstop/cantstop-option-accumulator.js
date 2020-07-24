90ed1ff7c83f8c3fee5db1f5374dfe0e4682762b
slightly-nerdy <github@slightlynerdy.org>
2020-07-24 08:55:58 +0100

    Remove old option accumlator

--- games/cantstop/cantstop-option-accumulator.js
+++ games/cantstop/cantstop-option-accumulator.js
@@ -1,57 +1,5 @@
 'use strict';
 
-// TO DO:  Remove after testing the new CantStopOptionAccumulator
-class CantStopOptionAccumulator_OLD {
-    constructor(in_play, is_full) {
-        this.in_play = in_play;
-        this.is_full = is_full;
-        this.options = new Array;
-    }
-    candidate_pair(a, b) {
-        if (this.is_full.has(a)) {
-            this.candidate_single(b);
-        }
-        else if (this.is_full.has(b)) {
-            this.candidate_single(a);
-        }
-        else {
-            // Find that number of columns that would be precommitted if both
-            // a and b are accepted;
-            let tp = this.in_play.size;
-            if (!this.in_play.has(a))
-                ++tp;
-            if (a != b && !this.in_play.has(b))
-                ++tp;
-            if (tp <= in_play_column_limit) {
-                // Both a and b can be accepted
-                this.options.push([a, b].sort());
-            }
-            else if (tp == in_play_column_limit + 1) {
-                // Both a and b can't be accepted, but it might be possible
-                // to accept one of them.
-                this.candidate_single(a);
-                this.candidate_single(b);
-            }
-        }
-    }
-    candidate_single(a) {
-        if (this.is_full.has(a)) {
-            // do nothing
-        }
-        else if (this.in_play.has(a)) {
-            this.options.push([a]);
-        }
-        else if (this.in_play.size < in_play_column_limit) {
-            this.options.push([a]);
-        }
-    }
-    // Unsorted, and could contain duplicates.
-    // Individual pairs are sorted.
-    get_options() {
-        return this.options;
-    }
-}
-
 class CantStopOptionAccumulator {
     constructor(in_play, is_full) {
         this.in_play = in_play;
